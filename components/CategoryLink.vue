5a5b560aaf26d7d5576f4fa480106eecc0a8e2de
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-02-09 16:27:47 -0500

    lesson finished

--- components/CategoryLink.vue
+++ components/CategoryLink.vue
@@ -5,29 +5,29 @@ const { category } = defineProps<{ category: Category }>()
 </script>
 
 <template>
-  <div class="category">
-    <a href="#">Category: {{ category.name }}</a>
-  </div>
+    <div class="category">
+        <NuxtLink :to="`/categories/${category.slug}`">Category: {{ category.name }}</NuxtLink>
+    </div>
 </template>
 
 <style scoped lang="scss">
 @use '@/assets/styles/colors.scss';
 
 .category {
-  display: block;
-  margin: 15px 0px;
+    display: block;
+    margin: 15px 0px;
 }
 
 .category a {
-  color: colors.$green-medium;
-  border: 1px solid colors.$green-dark;
-  border-radius: 5px;
-  font-size: 12pt;
-  padding: 5px 10px;
+    color: colors.$green-medium;
+    border: 1px solid colors.$green-dark;
+    border-radius: 5px;
+    font-size: 12pt;
+    padding: 5px 10px;
 }
 
 .category a:hover {
-  color: colors.$green-light;
-  border-color: colors.$green-medium;
+    color: colors.$green-light;
+    border-color: colors.$green-medium;
 }
 </style>
---
+++ pages/categories/[category].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { CategoryDetails } from '~/data/categories'
+import { getCategoryDetailsUrl } from '~/data/categories'
+const categorySlug = useParam('category')
+const { data: category } = await useFetch<CategoryDetails>(
+  getCategoryDetailsUrl(categorySlug)
+)
+</script>
+<template>
+    <main>
+        <template v-if="category">
+            <h1>
+                {{ category.name }}
+            </h1>
+            <PostCard
+                v-for="post in category.posts"
+                :key="post.id"
+                :post="post"
+            />
+        </template>
+    </main>
+</template>
